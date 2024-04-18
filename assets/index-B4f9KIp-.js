8efa6d01e51061955d491366a07bfa52e7335246
CraneReturn <127602190+CraneReturn@users.noreply.github.com>
2024-04-18 13:50:45 +0000

    Deploying to gh-pages from @ Hazi7/live-platform@9f5b4bac67fb5df4bd6a0c43e0c754e5e17701e5 🚀

--- assets/index-B4f9KIp-.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{o as c,c as n}from"./index-CqV0JbHB.js";const o={};function r(t,a){return c(),n("h1",null,"这是admin")}const f=e(o,[["render",r]]);export{f as default};
