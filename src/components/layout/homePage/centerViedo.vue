26c4ef4f201dc356e35a1bca4c202fcef60e2e58
CraneReturn <1066063143@qq.com>
2024-04-18 21:43:16 +0800

    导航栏

---
+++ src/components/layout/homePage/centerViedo.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="centerViedo">
+        <div class="centerViedoshow">
+            <ul>
+                <li class="listShow" >
+                    <img class="listFlgur" src="/src/assets/images/viedoImg/1.jpg">
+                </li>
+                <li class="listShow">
+                    <img class="listFlgur" src="/src/assets/images/viedoImg/2.jpg">
+                </li>
+                <li class="listShow" >
+                    <img class="listFlgur" src="/src/assets/images/viedoImg/3.jpg">
+                </li>
+                <li class="listShow" >
+                    <img class="listFlgur" src="/src/assets/images/viedoImg/2.jpg">
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+  
+  <script lang="ts">
+export default {};
+</script>
+  <script setup lang="ts">
+import { reactive, ref } from 'vue';
+  const viedoObjArr=ref([{
+    viedo:'',
+    img:"/src/assets/images/viedoImg/1.jpg"
+  },{
+    viedo:'',
+    img:"/src/assets/images/viedoImg/2.jpg"
+  },{
+    viedo:'',
+    img:"/src/assets/images/viedoImg/3.jpg"
+  }])
+</script>
+  <style lang="scss">
+@import "@/assets/style/homePage/homePageTop.scss";
+@import "@/assets/style/homePage/centerViedo.scss";
+@import "@/assets/style/component/component.scss";
+</style>
--- src/views/homePage.vue
+++ src/views/homePage.vue
@@ -1,12 +1,15 @@
 <template>
   <nailTopVue/>
+  <centerViedoVue/>
 </template>
 
 <script lang="ts">
+import centerViedoVue from '../components/layout/homePage/centerViedo.vue'
 import nailTopVue from '../components/layout/Nail/nailTop.vue'
 export default {
     components:{
-        nailTopVue
+        nailTopVue,
+        centerViedoVue
     }
 }
 </script>
