922258c22b6fb21dc31c3dec5bfa52bc7c8695e1
Hazi7 <65474900+Hazi7@users.noreply.github.com>
2024-04-19 01:00:43 +0000

    Deploying to gh-pages from @ Hazi7/live-platform@dbc8ab9452b8c13c330cdee37ea48ecf729b17ca 🚀

---
+++ assets/index-Dp80Agrw.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{o as c,c as n}from"./index-5pio5dvJ.js";const o={};function r(t,a){return c(),n("h1",null,"这是admin")}const f=e(o,[["render",r]]);export{f as default};
