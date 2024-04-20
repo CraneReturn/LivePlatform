8a94aca338bbad930ace477223051f2bdb3a7d02
CraneReturn <1066063143@qq.com>
2024-04-20 22:30:39 +0800

    没想好热门主播这一部分动画怎么写

--- src/views/client/homePage.vue
+++ src/views/client/homePage.vue
@@ -1,12 +1,14 @@
 <template>
-    <centerViedo />
-    <clientCommentVue />
-    <programeLike/>
+  <centerViedo />
+  <clientCommentVue />
+  <programeLike />
+  <hotanchorPerson/>
 </template>
 
 <script lang="ts" setup>
 import centerViedo from "./layouts/homePage/centerVideo.vue";
 import clientCommentVue from "./layouts/homePage/clientComment.vue";
 import programeLike from "./layouts/homePage/programeLike.vue";
+import hotanchorPerson from "./layouts/homePage/hotanchorPerson.vue";
 </script>
 <style></style>
