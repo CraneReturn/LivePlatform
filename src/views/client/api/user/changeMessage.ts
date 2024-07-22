e10298e77bf22a025ec8c37652ad1c0b9cd9f3e6
CraneReturn <1066063143@qq.com>
2024-07-22 19:56:46 +0800

    增加了类型 但是还没测试

---
+++ src/views/client/api/user/changeMessage.ts
@@ -0,0 +1,32 @@
+import service from "@/utils/request";
+export function getUsermessages() {
+  return service({
+    url: `/user/getUserInfo`,
+    headers: {
+      isToken: true
+    },
+    method: 'get',
+  })
+}
+export function changeUsermessages(userid:any,nickname:string,sex:string,birthday,intr,avatar) {
+    const date = new Date(birthday);
+    const newdate=date.toISOString();    
+    console.log(userid,'qqqqqq');
+    
+    return service({
+      url: `/user/update`,
+      headers: {
+        isToken: true
+      },
+      data:{
+        nickname,
+        birthday:newdate,
+        intr,
+        sex,
+        avatar,
+        userId:userid
+        
+      },
+      method: 'put',
+    })
+  }
