5118277044f0d2013f633ebec79a991eab88e166
CraneReturn <1066063143@qq.com>
2024-07-22 16:20:06 +0800

    上传视频基本逻辑写完了

--- src/views/client/layouts/videoCreate/ffmpegVue.vue
+++ src/views/client/layouts/videoCreate/ffmpegVue.vue
@@ -1,28 +1,38 @@
    <template>
-  <el-dialog v-model="propsList.dialogVisible" title="封面设定" width="50%">
+  <p class="changePhotospan" @click="dialogVisible = true">更改封面</p>
+  <el-dialog
+    v-model="dialogVisible"
+    title="视频封面"
+    width="600"
+    :before-close="handleClose"
+  >
     <div class="changeCoverTitleNail">
-      <span :class="{spliceFlagShow:spliceFlag}" @click="spliceFlag=true">
+      <span :class="{ spliceFlagShow: spliceFlag }" @click="spliceFlag = true">
         从视频中截取
       </span>
-      <span  :class="{spliceFlagShow:!spliceFlag}" @click="spliceFlag=false">
+      <span
+        :class="{ spliceFlagShow: !spliceFlag }"
+        @click="spliceFlag = false"
+      >
         自定义上传封面
       </span>
     </div>
     <div class="changecoverMain" v-show="spliceFlag">
       <p class="titleAnalysis">拖拽选框裁剪</p>
       <div class="photoCoverSelect">
-        <div class="photoCoverSelected">
-          <img :src="propsList.spliceImgList[0].img" alt="" ref="faceImg" />
+        <div class="photoCoverSelected" v-if="ruleForm.list">
+          <img :src="coverImg" alt="" ref="faceImg" />
         </div>
         <div class="userPhotoListSelect">
           <div class="leftBackInner"></div>
-          <div v-if="propsList.spliceImgList">
+          <div v-if="ruleForm.list">
             <div
               class="photoList"
-              v-for="(item, index) in propsList.spliceImgList"
+              v-for="(item, index) in ruleForm.list"
               :key="item.url"
             >
-              <img :src="item.img" alt="" @click="changeFaceImg(index)" />
+              <img :src="item.img" alt=""
+               @click="changeFaceImg(index)" />
             </div>
           </div>
         </div>
@@ -31,74 +41,126 @@
     <div class="customChangPhoto" v-show="!spliceFlag">
       <div class="customChangPhotocenter">
         <div class="customChangPhotocentericon">
-          <svg t="1715605866029" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2008" width="50" height="50"><path d="M565.333333 779.914667l51.445334-54.912a31.733333 31.733333 0 0 1 45.226666-1.226667 32.64 32.64 0 0 1 1.216 45.770667l-97.418666 104a37.034667 37.034667 0 0 1-52.821334 1.397333l-108.362666-104.202667a32.64 32.64 0 0 1-1.152-45.770666 31.733333 31.733333 0 0 1 45.248-1.173334L501.333333 774.421333V512.074667c0-17.877333 14.325333-32.373333 32-32.373334s32 14.506667 32 32.373334v267.84zM512 138.666667c123.018667 0 228.213333 86.709333 259.424 206.88C864.298667 347.146667 938.666667 426.090667 938.666667 522.688c0 97.6-75.914667 177.173333-170.133334 177.173333-17.674667 0-32-14.496-32-32.373333 0-17.877333 14.325333-32.373333 32-32.373333 58.357333 0 106.133333-50.08 106.133334-112.426667 0-62.336-47.776-112.416-106.133334-112.416-5.856 0-11.626667 0.501333-17.301333 1.482667-17.621333 3.050667-34.304-9.098667-37.024-26.986667C698.346667 280.693333 612.714667 203.424 512 203.424c-73.834667 0-140.928 41.536-177.376 107.861333a31.914667 31.914667 0 0 1-30.122667 16.576 140.373333 140.373333 0 0 0-9.568-0.32c-80.149333 0-145.6 68.586667-145.6 153.781334 0 85.184 65.450667 153.792 145.6 153.792 17.674667 0 32 14.496 32 32.373333 0 17.877333-14.325333 32.373333-32 32.373333C178.912 699.861333 85.333333 601.770667 85.333333 481.322667c0-118.314667 90.293333-215.061333 203.456-218.453334C338.090667 186.24 421.013333 138.666667 512 138.666667z" fill="#bfbfbf" p-id="2009"></path></svg>
-          <p>
-            请选择上传的封面
-          </p>
-          <input type="file" name="" id="" class="customChangPhotocenterInput" @change="customChangcover">
+          <svg
+            t="1715605866029"
+            class="icon"
+            viewBox="0 0 1024 1024"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            p-id="2008"
+            width="50"
+            height="50"
+          >
+            <path
+              d="M565.333333 779.914667l51.445334-54.912a31.733333 31.733333 0 0 1 45.226666-1.226667 32.64 32.64 0 0 1 1.216 45.770667l-97.418666 104a37.034667 37.034667 0 0 1-52.821334 1.397333l-108.362666-104.202667a32.64 32.64 0 0 1-1.152-45.770666 31.733333 31.733333 0 0 1 45.248-1.173334L501.333333 774.421333V512.074667c0-17.877333 14.325333-32.373333 32-32.373334s32 14.506667 32 32.373334v267.84zM512 138.666667c123.018667 0 228.213333 86.709333 259.424 206.88C864.298667 347.146667 938.666667 426.090667 938.666667 522.688c0 97.6-75.914667 177.173333-170.133334 177.173333-17.674667 0-32-14.496-32-32.373333 0-17.877333 14.325333-32.373333 32-32.373333 58.357333 0 106.133333-50.08 106.133334-112.426667 0-62.336-47.776-112.416-106.133334-112.416-5.856 0-11.626667 0.501333-17.301333 1.482667-17.621333 3.050667-34.304-9.098667-37.024-26.986667C698.346667 280.693333 612.714667 203.424 512 203.424c-73.834667 0-140.928 41.536-177.376 107.861333a31.914667 31.914667 0 0 1-30.122667 16.576 140.373333 140.373333 0 0 0-9.568-0.32c-80.149333 0-145.6 68.586667-145.6 153.781334 0 85.184 65.450667 153.792 145.6 153.792 17.674667 0 32 14.496 32 32.373333 0 17.877333-14.325333 32.373333-32 32.373333C178.912 699.861333 85.333333 601.770667 85.333333 481.322667c0-118.314667 90.293333-215.061333 203.456-218.453334C338.090667 186.24 421.013333 138.666667 512 138.666667z"
+              fill="#bfbfbf"
+              p-id="2009"
+            ></path>
+          </svg>
+          <p>请选择上传的封面</p>
+          <input
+            type="file"
+            name=""
+            id=""
+            class="customChangPhotocenterInput"
+            @change="customChangcover"
+          />
+          <img :src='coverImg' alt="">
         </div>
       </div>
     </div>
     <template #footer>
-      <span class="dialog-footer">
-        <el-button @click="toggleDialog">取消</el-button>
-        <el-button type="primary" @click="sureChangeFaceImg">确定</el-button>
-      </span>
+      <div class="dialog-footer">
+        <el-button @click="dialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="sureChangeFaceImgson">
+          确定
+        </el-button>
+      </div>
     </template>
   </el-dialog>
 </template>
   
   <script lang="ts" setup>
-import { ElMessageBox,ElMessage } from "element-plus";
-import { ref, defineProps, defineEmits } from "vue";
-import { MP4Clip } from "@webav/av-cliper";
+import { ElMessageBox, ElMessage } from "element-plus";
+import { ref, defineProps, defineEmits, onMounted } from "vue";
 const propsList = defineProps({
-  dialogVisible: Boolean,
   spliceImgList: Array as () => any[],
 });
-const spliceFlag=ref<boolean>(true)
-const emits = defineEmits(
-  ["update:dialogVisible","update:changeFaceImgshow"]
-  );
+import {
+  sendCoverimg, sendCoverimgframe
+} from "@/views/client/api/uploadFile/uploadFile";
+const spliceFlag = ref<boolean>(true);
+const emits = defineEmits(["update:dialogVisible", "update:sureChangeFaceImg"]);
+//集中处理
+import { storeToRefs } from "pinia";
+import videouploadMainStore from "@/store/video/videoUpload.ts";
+const mainvideoStore = videouploadMainStore();
+const { ruleForm, hasUploadedarr,nowindex,coverUrl } = storeToRefs(mainvideoStore);
 const faceImg = ref();
-const coverImg=ref()
+const coverImg = ref();
 const imgList: any = ref([]);
 const cost: any = ref(0);
-const toggleDialog = () => {
-  const newValue = false;
-  emits("update:dialogVisible", newValue);
+const dialogVisible = ref(false);
+const blobsend=ref(0)
+onMounted(()=>{
+  coverImg.value=hasUploadedarr.value[nowindex.value].list[0].img
+})
+const handleClose = (done: () => void) => {
+  ElMessageBox.confirm("确定要放弃选择的封面么?")
+    .then(() => {
+      done();
+    })
+    .catch(() => {
+      // catch error
+    });
 };
 //换封面
 const changeFaceImg = (index: number) => {
-  if (propsList.spliceImgList) {
-    faceImg.value.src = propsList.spliceImgList[index].img;
-    coverImg.value=propsList.spliceImgList[index].img;
+  if (ruleForm.list) {
+    faceImg.value.src = ruleForm.list[index].img;
+    coverImg.value = ruleForm.list[index].img;
   }
+  blobsend.value=index
+  const blobimg = ruleForm.value.listBlob[blobsend.value].img;
+const render = new FileReader();
+render.readAsDataURL(blobimg);
+render.onload =async function(event) {
+  const base64String = event.target.result;
+  const senddata=await sendCoverimgframe(base64String)
+    coverImg.value=senddata.data
+}
 };
-const customChangcover=((event:any)=>{
-  const file=event.target.files[0]
+const customChangcover =async (event: any) => {
+  const file = event.target.files[0];
   if (event.target.files[0].type != "image/png") {
-      ElMessage.error('请您上传图片文件')
-      return;
+    ElMessage.error("请您上传图片文件");
+    return;
   }
-  coverImg.value=file
-  console.log(coverImg);
+  const formdata=new FormData()
+  formdata.append('file',file)
+  const filedata=await sendCoverimg(formdata)
+  console.log(filedata);
   
-  
-})
-const sureChangeFaceImg = () => {
-  ElMessageBox.confirm("是否确定将该图片设为封面", {
-    confirmButtonText: "确定",
-    cancelButtonText: "取消",
-    type: "success",
-  })
-    .then(() => {
-      const newValueshow = false;
-      emits("update:dialogVisible", newValueshow);
-      emits("update:changeFaceImgshow", coverImg.value);
-    })
-    .catch(() => {});
+  if(filedata.code==20000){
+    coverImg.value=filedata.data
+  }
+
 };
+const sureChangeFaceImgson = async () => {
+  try {
+    await ElMessageBox.confirm("是否确定将该图片设为封面", {
+      confirmButtonText: "确定",
+      cancelButtonText: "取消",
+      type: "success",
+    })
+    if(coverImg.value!=''){
+      ruleForm.value.coverUrl=coverImg.value
+    }
+} catch (error) {
+    console.error("Error:", error);
+  }
+};
+
 </script>
   
   <style lang="scss">
@@ -113,20 +175,30 @@ const sureChangeFaceImg = () => {
 .text-center {
   text-align: center;
 }
-.changeCoverTitleNail{
-  span{
+.changeCoverTitleNail {
+  span {
     margin-left: 10px;
     cursor: pointer;
   }
-
 }
-.spliceFlagShow{
-  border-bottom: 1px solid var( --el-color-primary);
-  padding-bottom:5px ;
+.spliceFlagShow {
+  border-bottom: 1px solid var(--el-color-primary);
+  padding-bottom: 5px;
   color: var(--el-color-primary);
 }
-.changecoverMain{
+.changecoverMain {
   margin-top: 20px;
+}
+.customChangPhotocentericon{
+  position: relative;
+  img{
+    position: absolute;
+    top:-60px;
+    left: 0px;
+    width: 100%;
+    height: 100%;
+    z-index: 2;
+  }
 }
 </style>
   
--- src/views/client/layouts/videoCreate/uploadFillin.vue
+++ src/views/client/layouts/videoCreate/uploadFillin.vue
@@ -10,22 +10,30 @@
       :size="formSize"
       status-icon
     >
-      <el-form-item label="封面" prop="name">
-        <div class="fillInPhotoShow">
-          <img :src="spliceImgList[0].img" alt="" v-if="spliceImgList" ref="changefaceImgshowphoto"/>
-          <div class="changePhoto" @click="dialogVisible = true">
-            <p>更改封面</p>
+      <el-form-item label="封面" prop="coverUrl">
+        
+        <div class="fillInPhotoShow" v-if="ruleForm.list.length!=0">
+          <img :src="ruleForm.coverUrl" alt="" ref="changefaceImgshowphoto"/>
+          <div class="changePhoto">
+            <ffmpegVue 
+            :spliceImgList="spliceImgList"
+            @update:sureChangeFaceImg="changeFaceImgshowupdate" />
           </div>
         </div>
       </el-form-item>
-      <el-form-item label="标题" prop="name">
-        <el-input v-model="ruleForm.name" />
+      <el-form-item label="标题" prop="title">
+        <el-input v-model="ruleForm.title" />
       </el-form-item>
     <el-form-item label="分类" prop="count">
         <div class="block">
           <el-cascader
             v-model="ruleForm.count"
-            :options="optionstype"
+            :options="optionstype" @change="getDetailMore(ruleForm.count)"
+            ></el-cascader>
+            <el-cascader
+            v-model="ruleForm.count2"
+            :options="optionTypetype"
+            v-show="ruleForm.count==''"
             ></el-cascader>
         </div>
       </el-form-item> 
@@ -38,7 +46,7 @@
       <el-form-item label="视频介绍" prop="desc">
         <el-input v-model="ruleForm.desc" type="textarea" />
       </el-form-item>
-      <el-form-item label="是否定时" prop="delivery">
+      <!-- <el-form-item label="是否定时" prop="delivery">
         <el-switch v-model="ruleForm.delivery" />
       </el-form-item>
       <el-form-item label="选择日期(15天内)" v-show="ruleForm.delivery">
@@ -51,21 +59,15 @@
             value-format="x"
           />
         </el-form-item>
-      </el-form-item>
+      </el-form-item> -->
 
       <el-form-item>
-        <el-button type="primary" @click="submitForm(ruleFormRef)">
+        <el-button type="primary" @click="submitForm()">
           上传
         </el-button>
         <el-button @click="resetForm(ruleFormRef)">重置</el-button>
       </el-form-item>
     </el-form>
-    <ffmpegVue
-      :dialogVisible="dialogVisible"
-      @update:dialogVisible="handleDialogVisibleUpdate"
-      :spliceImgList="spliceImgList"
-      @update:changeFaceImgshow="changeFaceImgshowupdate"
-    />
   </div>
 </template>
 
@@ -74,9 +76,15 @@ import { reactive, ref, defineProps, watch, onMounted,onBeforeUnmount } from "vu
 import ffmpegVue from "./ffmpegVue.vue";
 import {
   getOneSort,
-  getTwoType
+  getTwoType,
+  uploadVideo
 } from "@/views/client/api/uploadFile/uploadFile";
-import type { ComponentSize, FormInstance, FormRules } from "element-plus";
+import { ElMessage, type ComponentSize, type FormInstance, type FormRules } from "element-plus";
+//集中处理
+import { storeToRefs } from "pinia";
+import videouploadMainStore from '@/store/video/videoUpload.ts'
+const mainvideoStore=videouploadMainStore()
+const {ruleForm,hasUploadedarr}=storeToRefs(mainvideoStore)
 const spliceImgList = ref();
 const changefaceImgshowphoto=ref()
 const props = defineProps({
@@ -101,131 +109,71 @@ watch(
     deep: true,
   }
 );
-interface RuleForm {
-  name: string;
-  region: string;
-  count: string;
-  date1: string;
-  date2: string;
-  delivery: boolean;
-  location: string;
-  type: string[];
-  resource: string;
-  desc: string;
-  makeUrl:string
-}
-const dialogVisible = ref<boolean>(false);
+
+// const dialogVisible = ref<boolean>(false);
 const formSize = ref<ComponentSize>("default");
 const ruleFormRef = ref<FormInstance>();
-const ruleForm = reactive<RuleForm>({
-  name: "Hello",
-  region: "",
-  count: "",
-  date1: "",
-  date2: "",
-  delivery: false,
-  location: "",
-  type: [],
-  resource: "",
-  desc: "",
-  makeUrl:''
-});
-let optionstype=reactive([])
+//存储所有数据
+const sendRuleForm=reactive([
+])
+
+let optionstype=ref([])
+let optionTypetype=ref([])
 onMounted(async()=>{
-  const sort=await getOneSort()
-  if(sort.data){
-    sort.data.forEach(async(e:any)=>{
-      e.value=e.sortId
-      e.label=e.sortName
-      const data=await getTwoType(e.value)
-     data.data.forEach((c:any)=>{
-      c.value=c.sortId
-      c.label=c.sortName
-     })
-     e.children=data.data
-      
+  const sort = await getOneSort()
+  if (sort.data) {
+    sort.data.forEach(async (e: { value: any; sortId: any; label: any; sortName: any; }) => {
+      e.value = e.sortId
+      e.label = e.sortName
     })
-    optionstype=sort.data
-    console.log(optionstype);
-    
+    optionstype.value=[...sort.data]
   }
   
 })
-const value = ref("");
-
+//更新分类第二个数据
+const getDetailMore=async(num: any[])=>{
+  const sendnum=num[0]
+  const data=await getTwoType(sendnum)
+  if (data.data) {
+    data.data.forEach((m) => {
+      m.value = m.sortId
+      m.label = m.sortName
+    })
+    optionTypetype.value=[...data.data]
+  }
+  
+}
 const typeViedo = ["自制", "转载",];
 
-const rules = reactive<FormRules<RuleForm>>({
-  name: [
-    { required: true, message: "请提供视频标题", trigger: "blur" },
-    { min: 0, max: 30, message: "长度需要小于30并且大于0", trigger: "blur" },
-  ],
-  count: [
-    {
-      required: true,
-      message: "请选择分类",
-      trigger: "change",
-    },
-  ],
-  date1: [
-    {
-      type: "date",
-      required: true,
-      message: "请选择正确日期",
-      trigger: "change",
-    },
-  ],
-  location: [
-    {
-      required: true,
-      message: "请选择视频来源",
-      trigger: "change",
-    },
-  ],
-  type: [
-    {
-      type: "array",
-      required: true,
-      message: "Please select at least one activity type",
-      trigger: "change",
-    },
-  ],
-  resource: [
-    {
-      required: true,
-      message: "Please select activity resource",
-      trigger: "change",
-    },
-  ],
-  desc: [
-    { required: true, message: "请提供视频介绍详细信息", trigger: "blur" },
-  ],
-  makeUrl: [
-    { },
-  ],
-});
+const submitForm = async () => {
+  if(!ruleForm.value){
+    ElMessage.error("请您上传完整信息");
+    return
+  }
+  const {title,coverUrl,region,count,count2,location,desc,makeUrl,md5,duration,url}=ruleForm.value
+  if(md5==''){
+    ElMessage.error("请您上传完整视频");
+  }else if(!title || !count2 ||!count || !location || !desc ){
+    ElMessage.error("请您上传完整视频信息");
+  }else if(location=='转载' && makeUrl==''){
+    ElMessage.error("请您填写视频转载地址");
+  }else{
+   const senddata=uploadVideo(count2[0],coverUrl,desc,makeUrl,duration,title,location,url)
+   console.log(senddata,'1111');
+   
+  }
 
-const submitForm = async (formEl: FormInstance | undefined) => {
-  if (!formEl) return;
-  await formEl.validate((valid, fields) => {
-    if (valid) {
-      console.log("submit!");
-    } else {
-      console.log("error submit!", fields);
-    }
-  });
 };
 
 const resetForm = (formEl: FormInstance | undefined) => {
   if (!formEl) return;
   formEl.resetFields();
 };
-const handleDialogVisibleUpdate = (value: boolean) => {
-  dialogVisible.value = value;
-};
 const changeFaceImgshowupdate= (value:any) => {
   changefaceImgshowphoto.value.src=value
 };
+//存储所有上传的数组
+
 //获取set
 </script>
 
