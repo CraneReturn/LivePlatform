a803fc48b7006f13b1aee47674f6ca099a3ce4fc
CraneReturn <1066063143@qq.com>
2024-07-23 16:06:25 +0800

    用户认证信息写完了

--- src/views/streamer/api/checkedMessage/index.ts
+++ src/views/streamer/api/checkedMessage/index.ts
@@ -4,9 +4,6 @@ export function uploadCheckmessage(name:any,carnum:any,fileList:[]) {
     fileList.forEach((f)=>{
         fileSend.push(JSON.stringify(f))
     })
-    console.log(JSON.stringify(fileSend),'aaaaaa');
-    console.log(name,carnum);
-    
   return service({
     url: `/user/auth`,
     headers: {
@@ -19,4 +16,31 @@ export function uploadCheckmessage(name:any,carnum:any,fileList:[]) {
         username: name
     }
   })
+}
+export function getCheckState() {
+  return service({
+    url: `/user/getAuthInfo`,
+    headers: {
+      isToken: true,
+    },
+    method: 'get',
+  })
+}
+export function updateChecked(name:any,carnum:any,fileList:[]) {
+  let fileSend=[]
+  fileList.forEach((f)=>{
+      fileSend.push(JSON.stringify(f))
+  })
+return service({
+  url: `/user/updateAuth`,
+  headers: {
+    isToken: true,
+  },
+  method: 'put',
+  data: {
+      cardImage: JSON.stringify(fileSend),
+      idCard: carnum,
+      username: name
+  }
+})
 }
